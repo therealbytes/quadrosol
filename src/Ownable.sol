29d3548e44f94e2e0aa608f3a7794358a53ed89d
bytes <therealbyt3s@gmail.com>
2023-01-19 19:26:13 +0100

    refactor: format

--- src/Ownable.sol
+++ src/Ownable.sol
@@ -2,15 +2,15 @@
 // https://github.com/latticexyz/mud/blob/main/packages/solecs/src/Ownable.sol
 pragma solidity >=0.8.0;
 
-import { Ownable as SolidStateOwnable } from "@solidstate/contracts/access/ownable/Ownable.sol";
-import { OwnableStorage } from "@solidstate/contracts/access/ownable/OwnableStorage.sol";
+import {Ownable as SolidStateOwnable} from "lib/solidstate-solidity/contracts/access/ownable/Ownable.sol";
+import {OwnableStorage} from "lib/solidstate-solidity/contracts/access/ownable/OwnableStorage.sol";
 
 /**
  * IERC173 implementation
  */
 contract Ownable is SolidStateOwnable {
-  constructor() {
-    // Initialize owner (SolidState has no constructors)
-    _setOwner(msg.sender);
-  }
+    constructor() {
+        // Initialize owner (SolidState has no constructors)
+        _setOwner(msg.sender);
+    }
 }
