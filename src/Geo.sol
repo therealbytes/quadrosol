75cf72cd8e9ee69923d9842125e88f8f505019ff
bytes <therealbyt3s@gmail.com>
2023-01-21 15:22:07 +0100

    feat: add math utils

--- src/Geo.sol
+++ src/Geo.sol
@@ -18,6 +18,44 @@ enum Quadrant {
     BOTTOM_RIGHT
 }
 
+library MathUtilsLib {
+    function max(uint256 a, uint256 b) public pure returns (uint256) {
+        return a > b ? a : b;
+    }
+
+    function min(uint256 a, uint256 b) public pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
+    function max(int256 a, int256 b) public pure returns (int256) {
+        return a > b ? a : b;
+    }
+
+    function min(int256 a, int256 b) public pure returns (int256) {
+        return a < b ? a : b;
+    }
+
+    function maxInt32(int32 a, int32 b) public pure returns (int32) {
+        return a > b ? a : b;
+    }
+
+    function minInt32(int32 a, int32 b) public pure returns (int32) {
+        return a < b ? a : b;
+    }
+}
+
+library PointLib {
+    function distanceSq(
+        Point memory a,
+        Point memory b
+    ) public pure returns (uint256) {
+        return
+            uint256(
+                int256((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y))
+            );
+    }
+}
+
 library RectLib {
     function contains(
         Rect memory rect,
@@ -40,6 +78,21 @@ library RectLib {
             other.min.x < rect.max.x &&
             rect.min.y < other.max.y &&
             other.min.y < rect.max.y;
+    }
+
+    function distanceSq(
+        Rect memory rect,
+        Point memory point
+    ) public pure returns (uint256) {
+        int32 xd = MathUtilsLib.maxInt32(
+            rect.min.x - point.x,
+            point.x - rect.max.x
+        );
+        int32 yd = MathUtilsLib.maxInt32(
+            rect.min.y - point.y,
+            point.y - rect.max.y
+        );
+        return uint256(int256(xd * xd + yd * yd));
     }
 
     function quadrant(
